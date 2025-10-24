8afcc1c707cde3cf3605abc14455170e58f72b35
Jais-Yadav <jaisyadav2020@gmail.com>
2025-10-24 19:23:16 +0530

    Time: 0 ms (100%), Space: 48 MB (18.06%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,18 +14,10 @@
  * }
  */
 class Solution {
-    public void count(TreeNode root,List<Integer> ls){
-            if(root==null)return;
-            ls.add(root.val);
-            count(root.left,ls);
-            count(root.right,ls);
-
-    }
+ 
     public int countNodes(TreeNode root) {
-        if(root==null)return 0;
-        List<Integer> ls=new ArrayList<>();
-        count(root,ls);
-        return ls.size();
+      if(root==null) return 0;
+      return countNodes(root.left)+countNodes(root.right)+1;
 
 
         
